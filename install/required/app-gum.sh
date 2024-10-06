1342e90505f196111dcc2dd24b207ed67ed311ef
Luca Pattocchio <pattocchioluca@gmail.com>
2024-10-07 00:29:58 +0200

    upd: required libraries now install first

---
+++ install/required/app-gum.sh
@@ -0,0 +1,7 @@
+# Gum is used for the Omakub commands for tailoring Omakub
+cd /tmp
+GUM_VERSION=$(curl -s "https://api.github.com/repos/charmbracelet/gum/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -sLo gum.deb "https://github.com/charmbracelet/gum/releases/download/v${GUM_VERSION}/gum_${GUM_VERSION}_amd64.deb"
+sudo dpkg -i gum.deb
+rm gum.deb
+cd -
