0a019ce5283762fe8b2eb4bce342ba2da0ae61a4
Luca Pattocchio <pattocchioluca@gmail.com>
2024-10-07 00:33:19 +0200

    fix: add a better neovim plugin installation;

--- install/terminal/select-dev-language.sh
+++ install/terminal/select-dev-language.sh
@@ -6,11 +6,24 @@ else
   languages=$(gum choose "${AVAILABLE_LANGUAGES[@]}" --no-limit --height 10 --header "Select programming languages")
 fi
 
+# Add programming languages to lazyvim
+enable_lazyvim_extras() {
+	local temp_file=$(mktemp /tmp/omakub.XXXXX)
+	local extras=("$@")
+	local jq_extras=$(printf '"%s",' "${extras[@]}")
+	jq_extras="[${jq_extras%,}]"
+
+	jq --argjson extras "$jq_extras" '.extras |= (. + $extras | unique)' ~/.config/nvim/lazyvim.json >"$temp_file" &&
+	mv "$temp_file" ~/.config/nvim/lazyvim.json
+}
+
 if [[ -n "$languages" ]]; then
   for language in $languages; do
     case $language in
     Node.js)
       mise use --global node@lts
+
+      enable_lazyvim_extras "lazyvim.plugins.extras.lang.typescript"
       ;;
     PHP)
       sudo add-apt-repository -y ppa:ondrej/php
@@ -18,6 +31,13 @@ if [[ -n "$languages" ]]; then
       php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
       php composer-setup.php --quiet && sudo mv composer.phar /usr/local/bin/composer
       rm composer-setup.php
+
+      # Configure PHP's LSP to use intelephense instead of phpactor (default)
+			if ! grep -q 'php_lsp' ~/.config/nvim/lua/config/options.lua; then
+				echo 'vim.g.lazyvim_php_lsp = "intelephense"' >>~/.config/nvim/lua/config/options.lua
+			fi
+
+      enable_lazyvim_extras "lazyvim.plugins.extras.lang.php" "lazyvim.plugins.extras.lang.typescript"
       ;;
     esac
   done
