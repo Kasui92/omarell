5ccf9c6edba7df2a0f25173934da208df0a6e2d4
Luca Pattocchio <pattocchioluca@gmail.com>
2025-04-19 23:05:14 +0200

    fix: missing file

--- applications/Omarell.sh
+++ applications/Omarell.sh
@@ -3,7 +3,7 @@ cat <<EOF >~/.local/share/applications/Omarell.desktop
 Version=1.0
 Name=Omarell
 Comment=Omarell Controls
-Exec=alacritty --config-file /home/$USER/.local/share/omakub/defaults/alacritty/pane.toml --class=Omakub --title=Omakub -e omakub
+Exec=alacritty --config-file /home/$USER/.config/alacritty/pane.toml --class=Omarell --title=Omarell -e omakub
 Terminal=false
 Type=Application
 Icon=/home/$USER/.local/share/omakub/applications/icons/Omarell.png
