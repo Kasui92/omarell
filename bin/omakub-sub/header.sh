4c766e224999f299d27f140cc6679199695a8363
Luca Pattocchio <pattocchioluca@gmail.com>
2025-04-19 23:51:46 +0200

    fix: adjust spacing in version display in header script

--- bin/omakub-sub/header.sh
+++ bin/omakub-sub/header.sh
@@ -2,5 +2,5 @@
 
 source $OMAKUB_PATH/ascii.sh
 echo "" # Add spacing
-echo "                                 $(cat $OMAKUB_PATH/version)"
+echo "                                       $(cat $OMAKUB_PATH/version)"
 echo "" # Add spacing
