29fc63c9802ad8d9f0d54e3eb884d105ae00b12d
Luca Pattocchio <pattocchioluca@gmail.com>
2025-04-19 22:53:06 +0200

    upd: Update to Omakub 1.2.1

---
+++ migrations/1745060743.sh
@@ -0,0 +1,17 @@
+cp ~/.local/share/omakub/configs/alacritty/shared.toml ~/.config/alacritty/shared.toml
+cp ~/.local/share/omakub/configs/alacritty/pane.toml ~/.config/alacritty/pane.toml
+cp ~/.local/share/omakub/configs/alacritty/btop.toml ~/.config/alacritty/btop.toml
+cp ~/.local/share/omakub/configs/alacritty.toml ~/.config/alacritty/alacritty.toml
+
+source $OMAKUB_PATH/applications/About.sh
+source $OMAKUB_PATH/applications/Activity.sh
+source $OMAKUB_PATH/applications/Neovim.sh
+source $OMAKUB_PATH/applications/Docker.sh
+source $OMAKUB_PATH/applications/Omarell.sh
+
+alacritty migrate 2>/dev/null || true
+alacritty migrate -c ~/.config/alacritty/pane.toml 2>/dev/null || true
+alacritty migrate -c ~/.config/alacritty/btop.toml 2>/dev/null || true
+
+# Turn off relative line numbers
+echo "vim.opt.relativenumber = false" >>~/.config/nvim/lua/config/options.lua
