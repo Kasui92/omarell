2d67d5ad5f101c0fe04089146e9af237f587f442
Luca Pattocchio <pattocchioluca@gmail.com>
2025-04-21 14:56:39 +0200

    fix: remove another hidden space,

--- bin/omakub-sub/update.sh
+++ bin/omakub-sub/update.sh
@@ -1,6 +1,6 @@
 CHOICES=(
 	"Omarell       Update Omarell itself and run any migrations"
-	"System        Update system packages"
+	"System	       Update system packages"
 	"Firmware      Update firmware packages"
 	"LazyGit       TUI for Git"
 	"LazyDocker    TUI for Docker"
