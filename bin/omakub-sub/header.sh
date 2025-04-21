494dd60fc5bde2931eaf7ae72719a5cf0da4870d
Luca Pattocchio <pattocchioluca@gmail.com>
2025-04-21 14:25:07 +0200

    fix: correct echo command for dev branch indicator in header script

--- bin/omakub-sub/header.sh
+++ bin/omakub-sub/header.sh
@@ -2,5 +2,5 @@
 
 source $OMAKUB_PATH/ascii.sh
 echo "" # Add spacing
-echo "                                       $(cat $OMAKUB_PATH/version) $(cd $OMAKUB_PATH && git branch --show-current | grep -q "dev" && echo "\033[0;33m(dev)\033[0m" || echo "")"
+echo "                                       $(cat $OMAKUB_PATH/version) $(cd $OMAKUB_PATH && git branch --show-current | grep -q "dev" && echo -e "\033[0;33m(dev)\033[0m" || echo "")"
 echo "" # Add spacing
