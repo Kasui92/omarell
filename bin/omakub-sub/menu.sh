81e85e01d1efcea44b62e382e1725c139626c897
Luca Pattocchio <pattocchioluca@gmail.com>
2025-04-21 14:51:01 +0200

    fix: remove redundant comments in menu script

--- bin/omakub-sub/menu.sh
+++ bin/omakub-sub/menu.sh
@@ -1,9 +1,6 @@
 
-# Controlla se è presente una nuova versione di Omarell dalle release di GitHub confrontando con version locale
 last_release=$(curl -s https://api.github.com/repos/Kasui92/omarell/releases/latest | grep tag_name | cut -d '"' -f 4 | sed 's/^v//' | tr -d '.')
 local_version=$(cat $OMAKUB_PATH/version | sed 's/^v//' | tr -d '.')
-
-# Se la versione online è maggiore di quella locale, mostra un messaggio di avviso
 if [ "$last_release" -gt "$local_version" ]; then
 	echo -e "\033[1;35mA new version of Omarell is available! \033[1m\033[32m($last_release)\033[0m"
 	echo "" # Add spacing
