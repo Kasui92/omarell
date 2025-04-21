886d01cdf9b5c60db3197d23f66569d1eac7350b
Luca Pattocchio <pattocchioluca@gmail.com>
2025-04-21 23:42:14 +0200

    fix: conditionally source vscode script based on optional dev apps

--- install/desktop/set-gnome-theme.sh
+++ install/desktop/set-gnome-theme.sh
@@ -7,12 +7,16 @@ if [[ -v OMAKUB_FIRST_RUN_THEME ]]; then
     source $OMAKUB_PATH/themes/$OMAKUB_FIRST_RUN_THEME/gnome.sh
     source $OMAKUB_PATH/themes/$OMAKUB_FIRST_RUN_THEME/gdm-background.sh
     source $OMAKUB_PATH/themes/$OMAKUB_FIRST_RUN_THEME/extensions.sh
-    source $OMAKUB_PATH/themes/$OMAKUB_FIRST_RUN_THEME/vscode.sh
+    if [[ "$OMAKUB_FIRST_RUN_OPTIONAL_DEV_APPS" == *"Visual-Studio-Code"* ]]; then
+        source $OMAKUB_PATH/themes/$OMAKUB_FIRST_RUN_THEME/vscode.sh
+    fi
 else
     source $OMAKUB_PATH/themes/tokyo-night/gnome.sh
     source $OMAKUB_PATH/themes/tokyo-night/gdm-background.sh
     source $OMAKUB_PATH/themes/tokyo-night/extensions.sh
-    source $OMAKUB_PATH/themes/tokyo-night/vscode.sh
+    if [[ "$OMAKUB_FIRST_RUN_OPTIONAL_DEV_APPS" == *"Visual-Studio-Code"* ]]; then
+        source $OMAKUB_PATH/themes/tokyo-night/vscode.sh
+    fi
 fi
 
 
