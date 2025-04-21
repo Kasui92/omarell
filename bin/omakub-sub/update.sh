02b01b735c2066bdc25d37c31290f4e7e987617c
Luca Pattocchio <pattocchioluca@gmail.com>
2025-04-21 14:41:35 +0200

    feat: add system and firmware update options to update script

--- bin/omakub-sub/update.sh
+++ bin/omakub-sub/update.sh
@@ -1,5 +1,7 @@
 CHOICES=(
 	"Omarell       Update Omarell itself and run any migrations"
+	"System	   	   Update system packages"
+	"Firmware      Update firmware packages"
 	"LazyGit       TUI for Git"
 	"LazyDocker    TUI for Docker"
 	"Neovim        Text editor that runs in the terminal"
@@ -17,6 +19,8 @@ else
 
 	case "$INSTALLER" in
 	"omarell") INSTALLER_FILE="$OMAKUB_PATH/bin/omakub-sub/migrate.sh" ;;
+	"system") INSTALLER_FILE="$OMAKUB_PATH/bin/omakub-sub/update-system.sh" ;;
+	"firmware") INSTALLER_FILE="$OMAKUB_PATH/bin/omakub-sub/update-firmware.sh" ;;
 	*) INSTALLER_FILE="$OMAKUB_PATH/install/terminal/app-$INSTALLER.sh" ;;
 	esac
 
