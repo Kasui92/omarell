2a9b19050808f4cc631e3d2b2447e4be860a8e63
Luca Pattocchio <pattocchioluca@gmail.com>
2025-04-21 14:22:26 +0200

    fix: add dev branch indicator to version display in header script

--- bin/omakub-sub/header.sh
+++ bin/omakub-sub/header.sh
@@ -2,5 +2,5 @@
 
 source $OMAKUB_PATH/ascii.sh
 echo "" # Add spacing
-echo "                                       $(cat $OMAKUB_PATH/version)"
+echo "                                       $(cat $OMAKUB_PATH/version) $([ $(git branch --show-current) == "dev" ] && echo "\033[0;33m(dev)\033[0m" || echo "")"
 echo "" # Add spacing
