9fec8f2e2dbb223f51296937939c81a42a7cd1ac
Luca Pattocchio <pattocchioluca@gmail.com>
2025-04-21 18:04:25 +0200

    fix: refactor version check logic for clarity and consistency

--- bin/omakub-sub/menu.sh
+++ bin/omakub-sub/menu.sh
@@ -1,8 +1,11 @@
+# This script check if there is a new version of Omarell available
+last_release_tag=$(curl -s https://api.github.com/repos/Kasui92/omarell/releases/latest | grep tag_name | cut -d '"' -f 4)
+last_release_number=$(echo $last_release_tag | sed 's/^v//' | tr -d '.')
+local_version_tag=$(cat $OMAKUB_PATH/version)
+local_version_number=$(echo $local_version_tag | sed 's/^v//' | tr -d '.')
 
-last_release=$(curl -s https://api.github.com/repos/Kasui92/omarell/releases/latest | grep tag_name | cut -d '"' -f 4 | sed 's/^v//' | tr -d '.')
-local_version=$(cat $OMAKUB_PATH/version | sed 's/^v//' | tr -d '.')
-if [ "$last_release" -gt "$local_version" ]; then
-	echo -e "\033[1;35mA new version of Omarell is available! \033[1m\033[32m($last_release)\033[0m"
+if [ "$last_release_number" -gt "$local_version_number" ]; then
+	echo -e "\033[1;35mA new version of Omarell is available! \033[1m\033[32m($last_release_tag)\033[0m"
 	echo "" # Add spacing
 fi
 
