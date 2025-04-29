e1d93412e86d23b8c0f31531b56f2e67dc1da456
Luca Pattocchio <pattocchioluca@gmail.com>
2025-04-29 22:08:10 +0200

    fix: ensure GDM background setting is only prompted for GNOME desktop (#1)

--- bin/omakub-sub/appearance/theme.sh
+++ bin/omakub-sub/appearance/theme.sh
@@ -2,19 +2,23 @@ THEME_NAMES=("Tokyo Night" "Catppuccin" "Nord" "Everforest" "Gruvbox" "Kanagawa"
 THEME=$(gum choose "${THEME_NAMES[@]}" "<< Back" --header "Choose your theme" --height 10 | tr '[:upper:]' '[:lower:]' | sed 's/ /-/g')
 
 if [ -n "$THEME" ] && [ "$THEME" != "<<-back" ]; then
-  SET_GDM_BACKGROUND=$(gum choose "Yes" "No" --header "Do you want to set the theme wallpaper as login background?" --height 5)
 
-  cp $OMAKUB_PATH/themes/$THEME/alacritty.toml ~/.config/alacritty/theme.toml
+  if [[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]]; then
+    SET_GDM_BACKGROUND=$(gum choose "Yes" "No" --header "Do you want to set the theme wallpaper as login background?" --height 5 | tr '[:upper:]' '[:lower:]')
+
+    cp $OMAKUB_PATH/themes/$THEME/alacritty.toml ~/.config/alacritty/theme.toml
+
+    source $OMAKUB_PATH/themes/$THEME/gnome.sh
+    if [ "$SET_GDM_BACKGROUND" == "yes" ]; then
+      source $OMAKUB_PATH/themes/$THEME/gdm-background.sh > /dev/null 2>&1
+    fi
+    source $OMAKUB_PATH/themes/$THEME/extensions.sh
+    source $OMAKUB_PATH/themes/$THEME/vscode.sh
+  fi
+
   cp $OMAKUB_PATH/themes/$THEME/zellij.kdl ~/.config/zellij/themes/$THEME.kdl
   sed -i "s/theme \".*\"/theme \"$THEME\"/g" ~/.config/zellij/config.kdl
   cp $OMAKUB_PATH/themes/$THEME/neovim.lua ~/.config/nvim/lua/plugins/theme.lua
-
-  source $OMAKUB_PATH/themes/$THEME/gnome.sh
-  if [ "$SET_GDM_BACKGROUND" == "Yes" ]; then
-    source $OMAKUB_PATH/themes/$THEME/gdm-background.sh > /dev/null 2>&1
-  fi
-  source $OMAKUB_PATH/themes/$THEME/extensions.sh
-  source $OMAKUB_PATH/themes/$THEME/vscode.sh
 
   gum spin --spinner globe --title "Theme changed!" -- sleep 3
 fi
