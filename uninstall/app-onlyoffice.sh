c7a803222e254afee570b56b2051d5677565a9c8
Luca Pattocchio <pattocchioluca@gmail.com>
2025-05-04 09:40:00 +0200

    upd: replace onlyoffice with libreoffice, for a clean installation

--- uninstall/app-onlyoffice.sh
+++
@@ -1,6 +0,0 @@
-# Uninstall onlyoffice
-sudo apt purge -y onlyoffice-desktopeditors
-
-# Remove repository
-sudo rm -f /etc/apt/sources.list.d/onlyoffice.list
-sudo rm -f /usr/share/keyrings/onlyoffice.gpg
