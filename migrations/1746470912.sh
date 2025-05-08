1be67c1d9a688c1eecb9354179e653edd84bb15f
Luca Pattocchio <pattocchioluca@gmail.com>
2025-05-08 14:27:06 +0200

    upd: add migration script for zellij themes

--- migrations/1746470912.sh
+++ migrations/1746470912.sh
@@ -1,3 +1,22 @@
 echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections
 sudo apt install ttf-mscorefonts-installer -y
 fc-cache
+
+# Overwrite old zellij themes with new ones
+if [ -d ~/.config/zellij/themes ]; then
+    files=()
+    for file in ~/.config/zellij/themes/*.kdl; do
+        if [[ -f "$file" ]]; then
+            files+=("$file")
+        fi
+    done
+
+    if [ ${#files[@]} -gt 0 ]; then
+        for file in "${files[@]}"; do
+            if [ -d "$OMAKUB_PATH/themes/$(basename "$file" .kdl)" ]; then
+                cp "$OMAKUB_PATH/themes/$(basename "$file" .kdl)"/zellij.kdl ~/.config/zellij/themes/$(basename "$file" .kdl).kdl
+            fi
+        done
+    fi
+fi
+
