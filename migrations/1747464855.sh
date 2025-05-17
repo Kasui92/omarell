b0d154799ccd03180ba49e2a76ef62f25cc44239
Luca Pattocchio <pattocchioluca@gmail.com>
2025-05-17 08:58:37 +0200

    fix:  add migration file (#12)

---
+++ migrations/1747464855.sh
@@ -0,0 +1,8 @@
+# Attempt to migrate btop config from omakub to btop if is not already set
+if [ ! -f "$HOME/.config/btop/btop.conf" ]; then
+  # Use Omakub btop config
+  mkdir -p ~/.config/btop
+  cp ~/.local/share/omakub/configs/btop.conf ~/.config/btop/btop.conf
+
+  echo -e "\033[1;35mThe btop config file has been copied to ~/.config/btop/btop.conf, now you can change Activity theme using Omarell.\033[0m"
+fi
