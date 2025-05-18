88a5e2f4ff7d1d8e911ffdd8b3e58e7e28584e3f
Luca Pattocchio <pattocchioluca@gmail.com>
2025-05-18 08:41:43 +0200

    fix:  prevent browser warning in manual option

--- bin/omakub-sub/manual.sh
+++ bin/omakub-sub/manual.sh
@@ -1,2 +1,4 @@
-xdg-open "https://manual.omakub.org" >/dev/null
+if ! xdg-open "https://manual.omakub.org" &>/dev/null; then
+  echo "Unable to open the manual in your default browser."
+fi
 source $OMAKUB_PATH/bin/omakub-sub/menu.sh
