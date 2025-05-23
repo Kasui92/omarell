d79566a37faa0c76b4fbfdbca982a715d632bab6
Luca Pattocchio <pattocchioluca@gmail.com>
2025-05-23 14:49:39 +0200

    fix: ensure btop folder creation and alacritty migration

--- migrations/1747464855.sh
+++ migrations/1747464855.sh
@@ -1,7 +1,7 @@
 # Attempt to migrate btop config from omakub to btop if is not already set
 if [ ! -f "$HOME/.config/btop/btop.conf" ]; then
   # Use Omakub btop config
-  mkdir -p ~/.config/btop
+  mkdir -p ~/.config/btop/themes
   cp ~/.local/share/omakub/configs/btop.conf ~/.config/btop/btop.conf
 
   echo -e "\033[1;35mThe btop config file has been copied to ~/.config/btop/btop.conf, now you can change Activity theme using Omarell.\033[0m"
