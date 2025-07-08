9ce95b282073c3203e0e9ea644c11b1c3166f47b
Luca Pattocchio <pattocchioluca@gmail.com>
2025-07-08 23:08:53 +0200

    fix: no chrome reference

--- themes/gruvbox/chrome.sh
+++
@@ -1,5 +0,0 @@
-CHROME_COLOR="-8847616"
-CHROME_COLOR_SCHEME=2
-CHROME_COLOR_VARIANT=2
-CHROME_BACKGROUND="gruvbox/background.png"
-source $OMARELL_PATH/themes/set-chrome-theme.sh
--- themes/tokyo-night/chrome.sh
+++
@@ -1,5 +0,0 @@
-CHROME_COLOR="3094106"
-CHROME_COLOR_SCHEME=2
-CHROME_COLOR_VARIANT=1
-CHROME_BACKGROUND="tokyo-night/background.jpg"
-source $OMARELL_PATH/themes/set-chrome-theme.sh
