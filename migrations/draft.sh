4b24b4a6075a825cd068115cbd6fde0fe0c203b7
Luca Pattocchio <pattocchioluca@gmail.com>
2025-07-11 17:48:53 +0200

    fix: improve Tactile

---
+++ migrations/draft.sh
@@ -0,0 +1,8 @@
+# Changes in Tactile
+gsettings set org.gnome.shell.extensions.tactile col-0 1
+gsettings set org.gnome.shell.extensions.tactile col-1 1
+gsettings set org.gnome.shell.extensions.tactile col-2 1
+gsettings set org.gnome.shell.extensions.tactile col-3 1
+gsettings set org.gnome.shell.extensions.tactile row-0 1
+gsettings set org.gnome.shell.extensions.tactile row-1 1
+gsettings set org.gnome.shell.extensions.tactile gap-size 10
