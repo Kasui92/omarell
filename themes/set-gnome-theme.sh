91c5630bc8a97d9dd96ee64e059722fb30e0d52a
Luca Pattocchio <pattocchioluca@gmail.com>
2025-07-12 01:04:19 +0200

    fix: missing EOL

--- themes/set-gnome-theme.sh
+++ themes/set-gnome-theme.sh
@@ -4,7 +4,7 @@ gsettings set org.gnome.desktop.interface gtk-theme "Yaru-$OMARELL_THEME_COLOR-d
 gsettings set org.gnome.desktop.interface icon-theme "Yaru-$OMARELL_THEME_COLOR"
 gsettings set org.gnome.desktop.interface accent-color "$OMARELL_THEME_COLOR" 2>/dev/null || true
 
-CURRENT_THEME_BACKGROUND="$(readlink -f "$HOME/.config/omarell/current/background")
+CURRENT_THEME_BACKGROUND="$(readlink -f "$HOME/.config/omarell/current/background")"
 
 gsettings set org.gnome.desktop.background picture-uri $CURRENT_THEME_BACKGROUND
 gsettings set org.gnome.desktop.background picture-uri-dark $CURRENT_THEME_BACKGROUND
