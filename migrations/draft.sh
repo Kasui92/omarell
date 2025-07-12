bc103a82b1b5370db13961a79d7750f4d5e3f9e7
Luca Pattocchio <pattocchioluca@gmail.com>
2025-07-12 08:45:58 +0200

    fix: use HOME path

--- migrations/draft.sh
+++ migrations/draft.sh
@@ -7,11 +7,11 @@ if [ -d "$HOME/.local/share/omakub" ]; then
 fi
 
 #
-source "~/.local/share/omarell/install/terminal/0-config.sh"
-source "~/.local/share/omarell/install/desktop/app-chromium.sh"
-source "~/.local/share/omarell/install/desktop/backgrounds.sh"
+source "$HOME/.local/share/omarell/install/terminal/0-config.sh"
+source "$HOME/.local/share/omarell/install/desktop/app-chromium.sh"
+source "$HOME/.local/share/omarell/install/desktop/backgrounds.sh"
 
-source "~/.local/share/omarell/default/bash/shell"
+source "$HOME/.local/share/omarell/default/bash/shell"
 
 # Install ffmpeg for video support
 sudo apt install -y ffmpeg
@@ -45,19 +45,19 @@ gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/or
 THEME_NAMES=("Tokyo Night" "Catppuccin" "Nord" "Everforest" "Gruvbox" "Kanagawa")
 OMARELL_FIRST_RUN_THEME=$(gum choose "${THEME_NAMES[@]}" ">> Skip" --header "Choose your current theme" --height 10 | tr '[:upper:]' '[:lower:]' | sed 's/ /-/g')
 
-source "~/.local/share/omarell/install/desktop/set-theme.sh"
+source "$HOME/.local/share/omarell/install/desktop/set-theme.sh"
 
 # Change desktop applications
-sudo rm -rf .local/share/applications/About.desktop
-sudo rm -rf .local/share/applications/Activity.desktop
-sudo rm -rf .local/share/applications/Neovim.desktop
-sudo rm -rf .local/share/applications/Docker.desktop
-sudo rm -rf .local/share/applications/Omarell.desktop
-source ~/.local/share/omarell/applications/About.sh
-source ~/.local/share/omarell/applications/Activity.sh
-source ~/.local/share/omarell/applications/Neovim.sh
-source ~/.local/share/omarell/applications/Docker.sh
-source ~/.local/share/omarell/applications/Omarell.sh
+sudo rm -rf "$HOME/.local/share/applications/About.desktop"
+sudo rm -rf "$HOME/.local/share/applications/Activity.desktop"
+sudo rm -rf "$HOME/.local/share/applications/Neovim.desktop"
+sudo rm -rf "$HOME/.local/share/applications/Docker.desktop"
+sudo rm -rf "$HOME/.local/share/applications/Omarell.desktop"
+source "$HOME/.local/share/omarell/applications/About.sh"
+source "$HOME/.local/share/omarell/applications/Activity.sh"
+source "$HOME/.local/share/omarell/applications/Neovim.sh"
+source "$HOME/.local/share/omarell/applications/Docker.sh"
+source "$HOME/.local/share/omarell/applications/Omarell.sh"
 
 # Autoremove unused packages
 sudo apt autoremove -y
