60f82bed554a1b86838fb95cc9ae796065c5609b
Luca Pattocchio <pattocchioluca@gmail.com>
2025-07-15 16:06:42 +0200

    upd: not use script for .desktop declaration, but copy them from omarell folders;

---
+++ install/desktop/xtras.sh
@@ -0,0 +1,22 @@
+# Optional Apps
+if [[ -v OMARELL_FIRST_RUN_OPTIONAL_APPS ]]; then
+	apps=$OMARELL_FIRST_RUN_OPTIONAL_APPS
+
+	if [[ -n "$apps" ]]; then
+		for app in $apps; do
+			source "$HOME/.local/share/omarell/install/desktop/optional/app-${app,,}.sh"
+		done
+	fi
+fi
+
+# Apps
+# Copy and sync icon files
+mkdir -p ~/.local/share/icons/hicolor/48x48/apps/
+cp ~/.local/share/omarell/applications/icons/*.png ~/.local/share/icons/hicolor/48x48/apps/
+gtk-update-icon-cache ~/.local/share/icons/hicolor &>/dev/null
+
+# Copy .desktop declarations
+mkdir -p ~/.local/share/applications
+cp ~/.local/share/omarell/applications/*.desktop ~/.local/share/applications/
+
+update-desktop-database ~/.local/share/applications
