362364e26e9a80fafbac694decae8fe7e5d61bb2
Luca Pattocchio <pattocchioluca@gmail.com>
2025-07-20 09:11:41 +0200

    ref: remove gnome-dock script , update keybindings, force rouded corner to fit with forge, and improve gnome-settings

--- install/desktop/gnome-extensions.sh
+++ install/desktop/gnome-extensions.sh
@@ -21,7 +21,7 @@ gext install tophat@fflewddur.github.io
 gext install AlphabeticalAppGrid@stuarthayhurst
 gext install gnome-ui-tune@itstime.tech
 gext install quick-settings-tweaks@qwreey
-gext install quick-settings-avatar@d-go
+gext install rounded-window-corners@fxgn
 
 # Compile gsettings schemas in order to be able to set them
 sudo cp ~/.local/share/gnome-shell/extensions/forge@jmmaranan.com/schemas/org.gnome.shell.extensions.forge.gschema.xml /usr/share/glib-2.0/schemas/
@@ -32,14 +32,19 @@ sudo cp ~/.local/share/gnome-shell/extensions/tophat@fflewddur.github.io/schemas
 sudo cp ~/.local/share/gnome-shell/extensions/AlphabeticalAppGrid\@stuarthayhurst/schemas/org.gnome.shell.extensions.AlphabeticalAppGrid.gschema.xml /usr/share/glib-2.0/schemas/
 sudo cp ~/.local/share/gnome-shell/extensions/gnome-ui-tune\@itstime.tech/schemas/org.gnome.shell.extensions.gnome-ui-tune.gschema.xml /usr/share/glib-2.0/schemas/
 sudo cp ~/.local/share/gnome-shell/extensions/quick-settings-tweaks\@qwreey/schemas/org.gnome.shell.extensions.quick-settings-tweaks.gschema.xml /usr/share/glib-2.0/schemas/
-sudo cp ~/.local/share/gnome-shell/extensions/quick-settings-avatar\@d-go/schemas/org.gnome.shell.extensions.quick-settings-avatar.gschema.xml /usr/share/glib-2.0/schemas/
+sudo cp ~/.local/share/gnome-shell/extensions/rounded-window-corners\@fxgn/schemas/org.gnome.shell.extensions.rounded-window-corners-reborn.gschema.xml /usr/share/glib-2.0/schemas/
 sudo glib-compile-schemas /usr/share/glib-2.0/schemas/
 
 # Configure Just Perfection
 gsettings set org.gnome.shell.extensions.just-perfection animation 2
-gsettings set org.gnome.shell.extensions.just-perfection dash-app-running true
+gsettings set org.gnome.shell.extensions.just-perfection search false
+gsettings set org.gnome.shell.extensions.just-perfection dash false
+gsettings set org.gnome.shell.extensions.just-perfection dash-separator false
+gsettings set org.gnome.shell.extensions.just-perfection dash-app-running false
+gsettings set org.gnome.shell.extensions.just-perfection show-apps-button false
 gsettings set org.gnome.shell.extensions.just-perfection workspace true
 gsettings set org.gnome.shell.extensions.just-perfection workspace-popup false
+gsettings set org.gnome.shell.extensions.just-perfection startup-status 0
 
 # Configure Blur My Shell
 gsettings set org.gnome.shell.extensions.blur-my-shell.appfolder blur false
@@ -49,7 +54,7 @@ gsettings set org.gnome.shell.extensions.blur-my-shell.window-list blur false
 gsettings set org.gnome.shell.extensions.blur-my-shell.panel blur false
 gsettings set org.gnome.shell.extensions.blur-my-shell.overview blur true
 gsettings set org.gnome.shell.extensions.blur-my-shell.overview pipeline 'pipeline_default'
-gsettings set org.gnome.shell.extensions.blur-my-shell.dash-to-dock blur true
+gsettings set org.gnome.shell.extensions.blur-my-shell.dash-to-dock blur false
 gsettings set org.gnome.shell.extensions.blur-my-shell.dash-to-dock brightness 0.6
 gsettings set org.gnome.shell.extensions.blur-my-shell.dash-to-dock sigma 30
 gsettings set org.gnome.shell.extensions.blur-my-shell.dash-to-dock static-blur true
@@ -58,8 +63,12 @@ gsettings set org.gnome.shell.extensions.blur-my-shell.dash-to-dock style-dash-t
 # Configure Space Bar
 gsettings set org.gnome.shell.extensions.space-bar.behavior smart-workspace-names false
 gsettings set org.gnome.shell.extensions.space-bar.shortcuts enable-activate-workspace-shortcuts false
-gsettings set org.gnome.shell.extensions.space-bar.shortcuts enable-move-to-workspace-shortcuts true
+gsettings set org.gnome.shell.extensions.space-bar.shortcuts enable-move-to-workspace-shortcuts false
 gsettings set org.gnome.shell.extensions.space-bar.shortcuts open-menu "@as []"
+gsettings set org.gnome.shell.extensions.space-bar.shortcuts activate-empty-key "@as []"
+gsettings set org.gnome.shell.extensions.space-bar.shortcuts activate-previous-key "@as []"
+gsettings set org.gnome.shell.extensions.space-bar.shortcuts move-workspace-left "@as []"
+gsettings set org.gnome.shell.extensions.space-bar.shortcuts move-workspace-right "@as []"
 
 # Configure TopHat
 gsettings set org.gnome.shell.extensions.tophat show-icons false
@@ -72,13 +81,14 @@ gsettings set org.gnome.shell.extensions.tophat network-usage-unit bits
 # Configure AlphabeticalAppGrid
 gsettings set org.gnome.shell.extensions.alphabetical-app-grid folder-order-position 'end'
 
-# Configure User Avatar In Quick Settings
-gsettings set org.gnome.shell.extensions.quick-settings-avatar avatar-position 1
-
 # Configure Forge
 gsettings set org.gnome.shell.extensions.forge window-gap-size 5
 gsettings set org.gnome.shell.extensions.forge window-gap-size-increment 1
 gsettings set org.gnome.shell.extensions.forge quick-settings-enabled false
 gsettings set org.gnome.shell.extensions.forge auto-exit-tabbed false
-gsettings set org.gnome.shell.extensions.forge focus-border-size 2
+gsettings set org.gnome.shell.extensions.forge focus-border-size 3
 gsettings set org.gnome.shell.extensions.forge tiling-mode-enabled true
+
+# Configure Rounded Window Corners
+gsettings set org.gnome.shell.extensions.rounded-window-corners-reborn border-width 0
+gsettings set org.gnome.shell.extensions.rounded-window-corners-reborn global-rounded-corner-settings "{'padding': <{'left': uint32 0, 'right': 0, 'top': 0, 'bottom': 0}>, 'keepRoundedCorners': <{'maximized': false, 'fullscreen': false}>, 'borderRadius': <uint32 14>, 'smoothing': <0.0>, 'borderColor': <(0.5, 0.5, 0.5, 1.0)>, 'enabled': <true>}"
