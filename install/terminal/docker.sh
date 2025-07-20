118f8e70e4d318ca3aef3ee177dbc5e0b1b16bf1
Luca Pattocchio <pattocchioluca@gmail.com>
2025-07-20 09:59:00 +0200

    upd: improve docker installation and add firewall

--- install/terminal/docker.sh
+++ install/terminal/docker.sh
@@ -1,15 +1,33 @@
 # Add the official Docker repo
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo wget -qO /etc/apt/keyrings/docker.asc https://download.docker.com/linux/ubuntu/gpg
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
+if ! command -v docker &>/dev/null; then
+  sudo install -m 0755 -d /etc/apt/keyrings
+  sudo wget -qO /etc/apt/keyrings/docker.asc https://download.docker.com/linux/ubuntu/gpg
+  sudo chmod a+r /etc/apt/keyrings/docker.asc
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt update
 
-# Install Docker engine and standard plugins
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
+  # Install Docker engine and standard plugins
+  sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
 
-# Give this user privileged Docker access
-sudo usermod -aG docker ${USER}
+  # Limit log size to avoid running out of disk
+  sudo mkdir -p /etc/docker
+  echo '{"log-driver":"json-file","log-opts":{"max-size":"10m","max-file":"5"}}' | sudo tee /etc/docker/daemon.json
 
-# Limit log size to avoid running out of disk
-echo '{"log-driver":"json-file","log-opts":{"max-size":"10m","max-file":"5"}}' | sudo tee /etc/docker/daemon.json
+  # Start Docker automatically
+  sudo systemctl enable docker
+
+  # Give this user privileged Docker access
+  sudo usermod -aG docker ${USER}
+
+  # Prevent Docker from preventing boot for network-online.target
+  sudo mkdir -p /etc/systemd/system/docker.service.d
+  sudo tee /etc/systemd/system/docker.service.d/no-block-boot.conf <<'EOF'
+[Unit]
+DefaultDependencies=no
+EOF
+
+  sudo systemctl daemon-reload
+fi
+
+
+
