f46cef7b028f12bfb0fbb13bdc2189b12ed5af1e
Luca Pattocchio <pattocchioluca@gmail.com>
2025-07-30 20:47:39 +0200

    fix: ensure alacritty config update is echoed during migration

--- migrations/1753871924.sh
+++ migrations/1753871924.sh
@@ -5,3 +5,6 @@ echo -e "\e[32m\nUpdate .bashrc...\e[0m"
 # Configure the bash shell using Omarell defaults
 [ -f ~/.bashrc ] && mv ~/.bashrc ~/.bashrc.bak
 cp ~/.local/share/omarell/default/bashrc ~/.bashrc
+
+echo -e "\e[32m\nUpdate alacritty config...\e[0m"
+cp -R ~/.local/share/omarell/config/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
