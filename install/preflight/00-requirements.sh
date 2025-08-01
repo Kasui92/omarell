922a5115f1040fdbd15c1ba905d65304fa4a69c0
Luca Pattocchio <pattocchioluca@gmail.com>
2025-08-01 23:08:17 +0200

    fix: update conditional checks to use double brackets for improved syntax

--- install/preflight/00-requirements.sh
+++ install/preflight/00-requirements.sh
@@ -9,7 +9,7 @@ fi
 . /etc/os-release
 
 # Check if running on Ubuntu 24.04 (Only LTS version supported)
-if [ "$ID" != "ubuntu" ] || [ $(echo "$VERSION_ID = 24.04" | bc) != 1 ]; then
+if [[ "$ID" != "ubuntu" ]] || [[ $(echo "$VERSION_ID = 24.04" | bc) != 1 ]]; then
   echo "$(tput setaf 1)Error: OS requirement not met"
   echo "You are currently running: $ID $VERSION_ID"
   echo "OS required: Ubuntu 24.04"
@@ -19,7 +19,7 @@ fi
 
 # Check if running on x86
 ARCH=$(uname -m)
-if [ "$ARCH" != "x86_64" ] && [ "$ARCH" != "i686" ]; then
+if [[ "$ARCH" != "x86_64" ]] && [[ "$ARCH" != "i686" ]]; then
   echo "$(tput setaf 1)Error: Unsupported architecture detected"
   echo "Current architecture: $ARCH"
   echo "This installation is only supported on x86 architectures (x86_64 or i686)."
