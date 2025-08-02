ce92a553037e80ff79d5247469c81a6a750f30dd
Luca Pattocchio <pattocchioluca@gmail.com>
2025-08-02 14:50:13 +0200

    fix: replace apt installation with pipx for tzupdate

--- install/config/50-timezones.sh
+++ install/config/50-timezones.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if ! command -v tzupdate &>/dev/null; then
-  sudo apt install -y tzupdate
+  pipx install tzupdate
   sudo tee /etc/sudoers.d/omarell-tzupdate >/dev/null <<EOF
 %wheel ALL=(root) NOPASSWD: /usr/bin/tzupdate, /usr/bin/timedatectl
 EOF
