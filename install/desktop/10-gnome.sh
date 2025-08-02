297156c3aef963ce38363a70517c943a628938ba
Luca Pattocchio <pattocchioluca@gmail.com>
2025-08-02 14:00:53 +0200

    fix: update GDM3 service management for proper boot configuration

--- install/desktop/10-gnome.sh
+++ install/desktop/10-gnome.sh
@@ -19,8 +19,32 @@ sudo apt install -y --no-install-recommends \
 # Use Wayland by default
 sudo sed -i 's/^#WaylandEnable=false/WaylandEnable=true/' /etc/gdm3/custom.conf
 
-# Enable GDM3
-sudo systemctl enable gdm3
+# Disable GDM3 if not needed
+sudo systemctl disable gdm3
+
+# Enable GDM3 on the next boot
+if [ ! -f /etc/systemd/system/omarell-enable-gdm.service ]; then
+  cat << 'EOF' | sudo tee /etc/systemd/system/omarell-enable-gdm.service
+[Unit]
+Description=Abilita GDM al primo boot post installazione
+After=multi-user.target
+
+[Service]
+Type=oneshot
+ExecStart=/bin/bash -c '
+  echo "[omarell] Abilitazione GDM post reboot..."
+  systemctl enable gdm3
+  rm -f /etc/systemd/system/omarell-enable-gdm.service
+  systemctl daemon-reexec
+'
+RemainAfterExit=no
+
+[Install]
+WantedBy=multi-user.target
+EOF
+fi
+
+sudo systemctl enable omarell-enable-gdm.service
 
 # Remove X11 packages if not needed
 sudo apt remove -y xserver-xorg* xserver-xorg* x11-* xinit xinput x11-common || true
