83227d1cea5676bafa9aa7b3e1c95d0911423449
Luca Pattocchio <pattocchioluca@gmail.com>
2025-08-03 01:50:57 +0200

    fix: ensure Forge theme is reapplied correctly in GNOME theme script

--- install/autostart/15-gnome-theme.sh
+++ install/autostart/15-gnome-theme.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Reapply Forge theme (remove old if exists)
+mkdir -p ~/.config/forge/stylesheet/forge
+if [ -f ~/.config/forge/stylesheet/forge/stylesheet.css ]; then
+  rm ~/.config/forge/stylesheet/forge/stylesheet.css
+fi
+ln -snf ~/.config/omarell/current/theme/forge.css ~/.config/forge/stylesheet/forge/stylesheet.css
+
 # Apply GNOME theme
 source ~/.config/omarell/current/theme/gnome.sh
 
