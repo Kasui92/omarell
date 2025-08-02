2dcd660a99306fa289b7332fdcd89cce6d4716c7
Luca Pattocchio <pattocchioluca@gmail.com>
2025-08-02 18:51:37 +0200

    fix: enhance network configuration by installing dbus

--- install/config/40-network.sh
+++ install/config/40-network.sh
@@ -1,36 +1,18 @@
 #!/bin/bash
 
 # Install and configure NetworkManager
-sudo apt install -y network-manager
+sudo apt install -y network-manager dbus
 
-# Create a service to switch from systemd-networkd to NetworkManager
-if [ ! -f /etc/systemd/system/omarell-network-switch.service ]; then
-  cat <<EOF | sudo tee /etc/systemd/system/omarell-network-switch.service
-[Unit]
-Description=Migrate from systemd-networkd to NetworkManager
-After=network.target
+# Disable systemd-networkd services
+sudo systemctl disable systemd-networkd.service systemd-networkd-wait-online.service
 
-[Service]
-Type=oneshot
-ExecStart=/bin/bash -c '
-  echo "[Omarell] Switching from systemd-networkd to NetworkManager..."
-  cp /etc/netplan/50-cloud-init.yaml /etc/netplan/50-cloud-init.yaml.bak
-  echo "network:
+# Create a basic netplan configuration file
+cp /etc/netplan/50-cloud-init.yaml /etc/netplan/50-cloud-init.yaml.bak
+cat <<EOF | sudo tee /etc/netplan/50-cloud-init.yaml
+network:
   version: 2
-  renderer: NetworkManager" > /etc/netplan/50-cloud-init.yaml
-  systemctl disable systemd-networkd.service systemd-networkd-wait-online.service
-  systemctl enable NetworkManager
-  netplan apply
-  echo "[Omarell] NetworkManager is now enabled and configured."
-  rm -f /etc/systemd/system/omarell-network-switch.service
-  systemctl daemon-reexec
-'
-RemainAfterExit=no
-
-[Install]
-WantedBy=multi-user.target
+  renderer: NetworkManager
 EOF
-fi
 
-# Enable the service to run on the next boot
-sudo systemctl enable omarell-network-switch.service
+# Enable NetworkManager service
+sudo systemctl enable NetworkManager
