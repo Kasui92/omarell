ee1ada3032ec5992cef2289efcb0ae416682a560
Luca Pattocchio <pattocchioluca@gmail.com>
2025-08-03 02:30:18 +0200

    feat: add hidden desktop entries for Flameshot, ImageMagick, and GNOME Screenshot

--- install/apps/10-xtras.sh
+++ install/apps/10-xtras.sh
@@ -18,18 +18,5 @@ if [ -z "$OMARELL_BARE" ]; then
 	done
 fi
 
-# Remove Btop entry for one that runs in alacritty
-sudo rm -rf /usr/share/applications/btop.desktop
-
-# App doesn't do anything when started from the app grid
-sudo rm -rf /usr/share/applications/org.flameshot.Flameshot.desktop
-
-# Remove the ImageMagick icon
-sudo rm -rf /usr/share/applications/display-im6.q16.desktop
-
-# We added our own meant for Alacritty
-sudo rm -rf /usr/local/share/applications/nvim.desktop
-sudo rm -rf /usr/local/share/applications/vim.desktop
-
 # Apps
 source ~/.local/share/omarell/bin/scripts/omarell-refresh-applications || true
