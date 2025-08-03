c47c3be3efe902546ba654bc4c2c0b5cc8310674
Luca Pattocchio <pattocchioluca@gmail.com>
2025-08-03 02:19:02 +0200

    feat: update GNOME theme scripts to include user-theme settings

--- themes/tokyo-night/gnome.sh
+++ themes/tokyo-night/gnome.sh
@@ -8,6 +8,7 @@ gsettings set org.gnome.desktop.interface cursor-theme 'Yaru'
 gsettings set org.gnome.desktop.interface gtk-theme "Yaru-$OMARELL_THEME_COLOR-dark"
 gsettings set org.gnome.desktop.interface icon-theme "Yaru-$OMARELL_THEME_COLOR"
 gsettings set org.gnome.desktop.interface accent-color "$OMARELL_THEME_COLOR" 2>/dev/null || true
+gsettings set org.gnome.shell.extensions.user-theme name "Yaru-$OMARELL_THEME_COLOR-dark"
 
 # Set GNOME extensions theme
 gsettings set org.gnome.shell.extensions.tophat meter-fg-color "#33ccff"
