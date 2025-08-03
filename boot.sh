dc57960905b08e1f6664a391c73bd6eb59e6335c
Luca Pattocchio <pattocchioluca@gmail.com>
2025-08-03 19:01:53 +0200

    refactor: update ASCII art and simplify boot script

--- boot.sh
+++ boot.sh
@@ -3,42 +3,26 @@
 set -o pipefail
 
 ascii_art='
-________                               .__  .__
-\_____  \   _____ _____ _______   ____ |  | |  |
- /   |   \ /     \\\__  \\\_  __ \_/ __ \|  | |  |
-/    |    \  Y Y  \/ __ \|  | \/\  ___/|  |_|  |__
-\_______  /__|_|  (____  /__|    \___  >____/____/
-        \/      \/     \/            \/
-
+ ██████╗ ███╗   ███╗ █████╗ ██████╗ ███████╗██╗     ██╗
+██╔═══██╗████╗ ████║██╔══██╗██╔══██╗██╔════╝██║     ██║
+██║   ██║██╔████╔██║███████║██████╔╝█████╗  ██║     ██║
+██║   ██║██║╚██╔╝██║██╔══██║██╔══██╗██╔══╝  ██║     ██║
+╚██████╔╝██║ ╚═╝ ██║██║  ██║██║  ██║███████╗███████╗███████╗
+ ╚═════╝ ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝
 '
 
-# Define the color gradient (shades of cyan and blue)
-colors=(
-  '\033[38;5;81m' # Cyan
-  '\033[38;5;75m' # Light Blue
-  '\033[38;5;69m' # Sky Blue
-  '\033[38;5;63m' # Dodger Blue
-  '\033[38;5;57m' # Deep Sky Blue
-  '\033[38;5;51m' # Cornflower Blue
-  '\033[38;5;45m' # Royal Blue
-)
-
-# Split the ASCII art into lines
-IFS=$'\n' read -rd '' -a lines <<<"$ascii_art"
-
-# Print each line with the corresponding color
-for i in "${!lines[@]}"; do
-  color_index=$((i % ${#colors[@]}))
-  echo -e "${colors[color_index]}${lines[i]}"
-done
-echo -e "\033[0m" # Reset color
+clear
+echo -e "\n$ascii_art\n"
 
 sudo apt-get update >/dev/null
 sudo apt-get install -y git >/dev/null
 
+# Use custom repo if specified, otherwise use default
+OMARELL_REPO="${OMARELL_REPO:-Kasui92/omarell}"
+
 echo -e "\e[32m\nCloning Omarell...\e[0m"
 rm -rf ~/.local/share/omarell
-git clone https://github.com/Kasui92/omarell.git ~/.local/share/omarell >/dev/null
+git clone https://github.com/$OMARELL_REPO.git ~/.local/share/omarell >/dev/null
 
 if [[ -n "$OMARELL_REF" ]]; then
   echo -e "\e[32mUsing branch: $OMARELL_REF\e[0m"
