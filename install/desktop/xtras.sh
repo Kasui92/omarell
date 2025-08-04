81590f9bb50ae8fa8447c354851da6a9b3f0bc85
Luca Pattocchio <pattocchioluca@gmail.com>
2025-08-04 09:47:15 +0200

    fix: add hidden .desktop entries for various applications instead of rm

--- install/desktop/xtras.sh
+++ install/desktop/xtras.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-# Remove Btop entry for one that runs in alacritty
-sudo rm -rf /usr/share/applications/btop.desktop
-
-# App doesn't do anything when started from the app grid
-sudo rm -rf /usr/share/applications/org.flameshot.Flameshot.desktop
-
-# Remove the ImageMagick icon
-sudo rm -rf /usr/share/applications/display-im6.q16.desktop
-
-# Replacing this with btop
-sudo rm -rf /usr/share/applications/org.gnome.SystemMonitor.desktop
-
-# We added our own meant for Alacritty
-sudo rm -rf /usr/local/share/applications/nvim.desktop
-sudo rm -rf /usr/local/share/applications/vim.desktop
-
 # Optional Apps
 if [[ -v OMARELL_FIRST_RUN_OPTIONAL_APPS ]]; then
 	apps=$OMARELL_FIRST_RUN_OPTIONAL_APPS
@@ -27,6 +11,5 @@ if [[ -v OMARELL_FIRST_RUN_OPTIONAL_APPS ]]; then
 	fi
 fi
 
-# Apps
-# Copy over Omarell applications
+# Refresh applications .desktop files
 source "$HOME/.local/share/omarell/bin/scripts/omarell-refresh-applications" || true
