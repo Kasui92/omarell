075a878d2d5d7a4e61712e6752988d3cc2b5aeb7
Luca Pattocchio <pattocchioluca@gmail.com>
2025-08-04 12:48:35 +0200

    fix: update Gum version to 0.16.2 in installation script

--- install/preflight/10-presentation.sh
+++ install/preflight/10-presentation.sh
@@ -5,7 +5,7 @@ export PATH="$PATH:$HOME/.local/bin"
 # Gum is used for the Omarell commands for tailoring Omarell after the initial install
 if ! command -v gum &> /dev/null; then
   cd /tmp
-  GUM_VERSION="0.14.3" # Use known good version
+  GUM_VERSION="0.16.2" # Tested Version
   wget -qO gum.deb "https://github.com/charmbracelet/gum/releases/download/v${GUM_VERSION}/gum_${GUM_VERSION}_amd64.deb"
   sudo apt-get install -y --allow-downgrades ./gum.deb
   rm gum.deb
