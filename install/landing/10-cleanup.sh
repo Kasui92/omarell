c43ee2438073a0850d019ea8bffab1f13d8c56a7
Luca Pattocchio <pattocchioluca@gmail.com>
2025-08-06 10:05:27 +0200

    feat: remove unused packages

--- install/landing/10-cleanup.sh
+++ install/landing/10-cleanup.sh
@@ -2,7 +2,14 @@
 
 # Update the package database
 sudo updatedb
+
 # Update and upgrade system packages
 sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y
+
+# Remove unnecessary packages and clean up
+sudo apt purge -y byobu snapd landscape-common unattended-upgrades \
+              cloud-init cloud-guest-utils \
+              apport apport-symptoms popularity-contest
+
 # Clean up unnecessary packages
 sudo apt autoremove -y && sudo apt clean
