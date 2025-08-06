8d4cb2313f04aa1967de69a49db6fb35cf26ac2c
Luca Pattocchio <pattocchioluca@gmail.com>
2025-08-06 09:34:12 +0200

    feat: add timezone update script and adjust sudoers configuration

--- install/config/50-timezones.sh
+++ install/config/50-timezones.sh
@@ -3,7 +3,7 @@
 if ! command -v tzupdate &>/dev/null; then
   pipx install tzupdate
   sudo tee /etc/sudoers.d/omarell-tzupdate >/dev/null <<EOF
-%wheel ALL=(root) NOPASSWD: /usr/bin/tzupdate, /usr/bin/timedatectl
+%wheel ALL=(root) NOPASSWD: $HOME/.local/bin/tzupdate, /usr/bin/timedatectl
 EOF
   sudo chmod 0440 /etc/sudoers.d/omarell-tzupdate
 fi
