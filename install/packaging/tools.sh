bbf61ac8352548d1f0ae99e0b60408d11498193a
Luca Pattocchio <pattocchioluca@gmail.com>
2025-09-03 09:46:16 +0200

    Add Zellij

--- install/packaging/tools.sh
+++ install/packaging/tools.sh
@@ -12,6 +12,14 @@ echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.gpg arch=$(dpkg --pr
 sudo apt update
 sudo apt install -y mise
 
+# Install Zellij
+cd /tmp
+wget -O zellij.tar.gz "https://github.com/zellij-org/zellij/releases/latest/download/zellij-x86_64-unknown-linux-musl.tar.gz"
+tar -xf zellij.tar.gz zellij
+sudo install zellij /usr/local/bin
+rm zellij.tar.gz zellij
+cd -
+
 # Install Starship
 source ~/.local/share/omakub/applications/install/starship.sh
 
